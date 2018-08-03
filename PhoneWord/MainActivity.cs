b21d0219b8f7c458d6fba8065b1da75f173f97c7
sdw <sdw@inea.com>
2018-08-03 14:01:04 -0400

    Initial commit

--- PhoneWord/MainActivity.cs
+++ PhoneWord/MainActivity.cs
@@ -14,6 +14,26 @@ namespace PhoneWord
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            // Add code to translate number
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
         }
     }
 }
